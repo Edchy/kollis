b08b964bcee1a63e1805695bf557b26124cf3bb5
“Edchrisnat” <edo_so@hotmail.com>
2023-12-18 11:24:41 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainHeader />
     </div>
   );
 }
 
-export default App;
+function MainHeader() {
+  return (
+    <header className="main-header">
+      <nav role="navigation" aria-label="Main Navigation" className="main-nav">
+        <div className="main-logo">Kollis</div>
+        <ul className="main-list">
+          <li>link</li>
+          <li>link</li>
+          <li>link</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+// async function fetchData(q) {
+//   const apiKey = "OnOOnBzJskNHQToKj+jVlg==7c0VISHeXUUmy9Sz";
+//   const apiUrl = "https://api.calorieninjas.com/v1/nutrition?query=" + q;
+
+//   try {
+//     const response = await fetch(apiUrl, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "X-Api-Key": apiKey,
+//       },
+//     });
+
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+
+//     const data = await response.json();
+
+//     console.log(data);
+//   } catch (error) {
+//     console.error("Error fetching data:", error.message);
+//   }
+// }
